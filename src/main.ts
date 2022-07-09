4ff95d243444d96f909c1082829d285327dc70d8
Arthuralentejo <arthur.alentejo228@gmail.com>
2022-07-09 01:46:28 -0300

    Criando lógica de desenho

--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,69 @@
 import './style.css'
 import Konva from 'konva';
-// const app = document.querySelector<HTMLDivElement>('#app')!
-
-// app.innerHTML = `
-//   <h1>Hello Vite!</h1>
-//   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-// `
+import { KonvaEventObject } from 'konva/lib/Node';
 
 const stage = new Konva.Stage({
   container: 'canva-holder',
-  width: 500,
-  height: 500,
+  width: 600,
+  height: 600,
 });
 
+const layer = new Konva.Layer().moveTo(stage);
+let rect: Konva.Rect | null = null;
+let isNowDrawing = false
 
-const layer = new Konva.Layer();
-stage.add(layer);
-let rect = new Konva.Rect({
-  x: 100,
-  y: 100,
-  fill: "blue",
-  width: 200,
-  height: 100,
-  stroke: "orange",
-  strokeWidth: 12,
-  cornerRadius: 12,
-  draggable:true
-})
-layer.add(rect)
-layer.draw()
+stage.on("mousedown", mousedownHandler)
+stage.on("mousemove", mousemoveHandler)
+stage.on("mouseup", mouseupHandler)
+
+function mousedownHandler(e: KonvaEventObject<MouseEvent> ){
+  
+  isNowDrawing = true;
+  rect = new Konva.Rect({
+    x: e.currentTarget.getRelativePointerPosition().x,
+    y: e.currentTarget.getRelativePointerPosition().y,
+    fill: "lightblue",
+    stroke: "blue",
+  });
+
+  layer.add(rect).batchDraw();
+}
+function mousemoveHandler(e){
+  if(!isNowDrawing) return false;
+
+  const newWidth = e.currentTarget.getRelativePointerPosition().x - rect.x();
+  const newHeight = e.currentTarget.getRelativePointerPosition().y - rect.y();
+  rect.width(newWidth);
+  rect.height(newHeight);
+  layer.batchDraw();
+}
+function mouseupHandler(){
+  isNowDrawing = false;
+}
+
+
+
+
+
+
+
+
+
+
+
+// stage.on('click',drawRect)
+// function drawRect(e) {
+//   if (drawning) {
+//     const rect = new Konva.Rect({
+//       x: stage.getPointerPosition().x,
+//       y: stage.getPointerPosition().y,
+//       width: 100,
+//       height: 100,
+//       fill: '#00D0FF',
+//       stroke: '#00D0FF',
+//       strokeWidth: 5,
+//     });
+//     layer.add(rect);
+//     stage.batchDraw();
+//   }
+// }
