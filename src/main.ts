caab2e0b4320d60b915ef32643b4e0cb4eab0d13
Arthuralentejo <arthur.alentejo228@gmail.com>
2022-07-11 23:23:55 -0300

    Transferindo lógica para a ferramenta de Retangulo

--- src/main.ts
+++ src/main.ts
@@ -1,69 +1,10 @@
 import './style.css'
 import Konva from 'konva';
-import { KonvaEventObject } from 'konva/lib/Node';
+import { RectTool } from './rectTool';
 
 const stage = new Konva.Stage({
   container: 'canva-holder',
-  width: 600,
-  height: 600,
+  width: window.innerWidth,
+  height: window.innerHeight,
 });
-
-const layer = new Konva.Layer().moveTo(stage);
-let rect: Konva.Rect | null = null;
-let isNowDrawing = false
-
-stage.on("mousedown", mousedownHandler)
-stage.on("mousemove", mousemoveHandler)
-stage.on("mouseup", mouseupHandler)
-
-function mousedownHandler(e: KonvaEventObject<MouseEvent> ){
-  
-  isNowDrawing = true;
-  rect = new Konva.Rect({
-    x: e.currentTarget.getRelativePointerPosition().x,
-    y: e.currentTarget.getRelativePointerPosition().y,
-    fill: "lightblue",
-    stroke: "blue",
-  });
-
-  layer.add(rect).batchDraw();
-}
-function mousemoveHandler(e){
-  if(!isNowDrawing) return false;
-
-  const newWidth = e.currentTarget.getRelativePointerPosition().x - rect.x();
-  const newHeight = e.currentTarget.getRelativePointerPosition().y - rect.y();
-  rect.width(newWidth);
-  rect.height(newHeight);
-  layer.batchDraw();
-}
-function mouseupHandler(){
-  isNowDrawing = false;
-}
-
-
-
-
-
-
-
-
-
-
-
-// stage.on('click',drawRect)
-// function drawRect(e) {
-//   if (drawning) {
-//     const rect = new Konva.Rect({
-//       x: stage.getPointerPosition().x,
-//       y: stage.getPointerPosition().y,
-//       width: 100,
-//       height: 100,
-//       fill: '#00D0FF',
-//       stroke: '#00D0FF',
-//       strokeWidth: 5,
-//     });
-//     layer.add(rect);
-//     stage.batchDraw();
-//   }
-// }
+RectTool.init(stage);
