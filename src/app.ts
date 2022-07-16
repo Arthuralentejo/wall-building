c0a6c2fa2339b82b29974b56f8fe715abc6ffc74
Arthuralentejo <arthur.alentejo228@gmail.com>
2022-07-15 21:05:57 -0300

    Feature: Criando nova animação de desenho

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,21 @@
 import Konva from "konva";
 import { KonvaEventObject } from "konva/lib/Node";
 import { ShapeConfig } from "konva/lib/Shape";
+import { Rect } from "konva/lib/shapes/Rect";
 import { ITool } from "./tools/ITool";
 import { WallTool } from "./tools/WallTool";
-import { IControls } from "./utils/IControls";
 
 export class App {
-  public stage: Konva.Stage;
-  public layers;
-  public tool: ITool;
-  public controls: IControls;
-  constructor(controls: IControls, stage?: Konva.Stage) {
-    this.controls = controls;
-    this.stage = stage || new Konva.Stage({
+  private stage: Konva.Stage;
+  private WALL_HEIGHT: number = 300;
+  private layers;
+  private shapes: Konva.Shape[] = [];
+  private isToolActive: boolean = false;
+  private button: HTMLElement | null;
+  public selected: Konva.Node | null = null;
+  private isAnimating: boolean = false;
+  constructor() {
+    this.stage = new Konva.Stage({
       container: 'canva-holder',
       width: window.innerWidth,
       height: window.innerHeight,
@@ -21,133 +24,68 @@ export class App {
       static: new Konva.Layer({ name: "static" }),
       drawing: new Konva.Layer({ name: "drawing" }),
     };
-    this.tool = new WallTool();
+    this.button = document.querySelector('.tool');
     this.stage.on("mousemove", this.mouseTracker);
   }
 
-  public activateTool(): void {
-    this.tool.isActive = !this.tool.isActive;
-    this.tool.icon.setAttr("opacity", this.tool.isActive ? 1 : 0.5);
-    document.body.style.cursor = this.tool.isActive ? "crosshair" : "default"
-    if (this.tool.isActive) {
-      this.stage.on("mousedown", (e) => {
-        e.cancelBubble = true;
-        this.tool.startDraw(e);
-        this.tool.shape?.moveTo(this.layers.drawing);
-        this.layers.drawing.batchDraw();
-        this.updateControls();
-      });
-      this.stage.on("mousemove", (e) => {
-        e.cancelBubble = true;
-        if (this.tool.isDrawing) {
-          this.tool.drawing(e);
-          this.layers.drawing.batchDraw();
-          this.updateControls();
-        }
-      });
-      this.stage.on("mouseup", (e) => {
-        e.cancelBubble = true;
-        this.tool.endDraw(e)
-        this.addShapeEventListeners();
-        this.layers.drawing.batchDraw();
-        document.body.style.cursor = "default"
-        this.stage.off("mousedown mouseup");
-        this.updateControls();
-      });
-    } else {
-      this.stage.off("mousedown mouseup");
+  private toggleTool(e): void {
+    console.log("Tipo do obj evento: " + typeof e);
 
+    this.isToolActive = !this.isToolActive;
+    e.currentTarget.classList.toggle("active");
+    document.body.style.cursor = this.isToolActive ? "crosshair" : "default"
+    if (this.isToolActive) {
+      this.stage.on("click", this.toggleAnimation);
+    } else {
+      this.stage.off("click");
     }
 
   }
-
-  private drawIcon() {
-
-    this.tool.icon.on("mouseover mouseout", (e) => {
-      if (!this.tool.isActive) {
-        const mouseover = e.type === "mouseover";
-        document.body.style.cursor = mouseover ? "pointer" : "default";
-        e.target.setAttr("opacity", mouseover ? 1 : 0.5);
-      }
-    });
-    this.tool.icon.on("click", (e) => {
-      e.cancelBubble = true;
-      this.activateTool()
-    })
-
-    this.layers.static.add(this.tool.icon);
-    this.layers.static.batchDraw();
+  private toggleAnimation(e: KonvaEventObject<MouseEvent>): void {
+    this.isAnimating = !this.isAnimating;
+    if (this.isAnimating) {
+      this.selected = e.target;
+      this.draw(e.target.getRelativePointerPosition());
+    } else{
+      this.selected = null;
+      this.endDraw(e.target.getRelativePointerPosition());
+    }
+  }
+  endDraw(location: {
+    x: number;
+    y: number;
+  }) {
+    throw new Error("Method not implemented.");
   }
 
   public init() {
     this.stage.add(this.layers.static, this.layers.drawing);
-    this.drawIcon();
-    this.stage.batchDraw();
-    this.addControlsEventListeners();
-  }
-
-  private addControlsEventListeners() {
-    this.controls.form.addEventListener('change', (e) => {
-      const target = e.target as HTMLInputElement;
-      this.tool.updateShape({ [target.name]: parseInt(target.value) })
-    });
-
-    this.controls.drawButton.addEventListener('click', (e) => {
-      e.preventDefault();
-      const config = this.controls.getControlsValues();      
-      this.draw(config);
-    });
-    this.controls.eraseButton.addEventListener('click', (e) => {
-      e.preventDefault();
-      this.tool.eraseDrawing();
-    });
-  }
-
-  public draw(config: ShapeConfig): void {
-    if (!this.tool.shape) {
-      this.tool.draw(config);
-    } else {
-      this.tool.updateShape(config);
-    }
-    this.addShapeEventListeners();
-    this.layers.drawing.add(this.tool.shape!);
+    this.button?.addEventListener("click", this.toggleTool);
     this.stage.batchDraw();
   }
 
-  private addShapeEventListeners() {
-    if (this.tool.shape) {
-      this.tool.shape.on("click", (e: KonvaEventObject<MouseEvent>) => {
-        let tr = new Konva.Transformer({
-          enabledAnchors: ['top-left', 'top-right', 'bottom-left', 'bottom-right'],
-        });
-
-        tr.nodes([this.tool.shape as Konva.Rect]);
-        e.target.getLayer().add(tr);
-        e.target.getLayer().batchDraw();
-      });
-      this.tool.shape.on("transform dragmove", (e: KonvaEventObject<MouseEvent>) => {
-
-        let target = e.target as Konva.Rect;
-        if (e.type === "transform") {
-          target.setAttrs({
-            width: target.width() * target.scaleX(),
-            height: target.height() * target.scaleY(),
-            scaleX: 1,
-          });
-        }
-        this.updateControls();
-      });
-
-    }
+  private draw(location: {
+    x: number;
+    y: number;
+  }): void {
+    const wall = new Konva.Rect({
+      height: this.WALL_HEIGHT,
+      width: 1,
+      x: location.x,
+      y: location.y,
+      fill: "lightblue",
+      stroke: "blue",
+      strokeWidth: .5,
+      draggable: true,      
+    })
+    this.addShape(wall);
   }
-  public updateControls() {
-    if (this.tool.shape) {
-      this.controls.width.value = Math.round(this.tool.shape.width()).toString();
-      this.controls.height.value = Math.round(this.tool.shape.height()).toString();
-      this.controls.x.value = Math.round(this.tool.shape.x()).toString();
-      this.controls.y.value = Math.round(this.tool.shape.y()).toString();
-    }
+  addShape(wall: Konva.Rect) {
+    this.layers.drawing.add(wall);
+    this.shapes.push(wall);
+    this.layers.drawing.batchDraw();
   }
+
 
   private mouseTracker(e: KonvaEventObject<MouseEvent>): void {
     const { x, y } = e.currentTarget.getRelativePointerPosition();
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import './style.css'
 import { App } from './app';
-import { Controls } from './utils/Controls';
 
-const controls = new Controls()
-const app = new App(controls);
+const app = new App();
 app.init();
+
